c5cd481f77514ff1ab3ca456e775ae7e61a0b437
Sander Mets <smets@malwarebytes.com>
2020-05-10 19:08:57 +0300

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import photopea from './img/photopea.png';
+import patikohv from './img/patikohv.png';
+import photoonwood from './img/photoonwood.png';
+import done_venice from './img/done_venice.png';
+import alexander from './img/alexander.png';
+import kehaline from './img/kehaline.jpg';
+import animation from './img/animation.gif';
 
 function App() {
   return (
     <AwesomeSlider animation="cubeAnimation">
       <div data-src={photopea} />
-      <div data-src="/img/patikohv.png" />
-      <div data-src="/img/photoonwood.png" />
-      <div data-src="/img/done_venice.png" />
-      <div data-src="/img/alexander.png" />
-      <div data-src="/img/kehaline.jpg" />
-      <div data-src="/img/animation.gif" />
+      <div data-src={patikohv} />
+      <div data-src={photoonwood} />
+      <div data-src={done_venice} />
+      <div data-src={alexander} />
+      <div data-src={kehaline} />
+      <div data-src={animation} />
     </AwesomeSlider>
   );
 }
