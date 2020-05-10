40837d600474c0f421dcc9f80d08b69eb577b93d
Sander Mets <smets@malwarebytes.com>
2020-05-10 18:57:23 +0300

    try adding pictures

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 // import './App.css';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
-
+import photopea from '/img/photopea.png';
 
 function App() {
   return (
     <AwesomeSlider animation="cubeAnimation">
-      <div data-src="/img/photopea.png" />
+      <div data-src={photopea} />
       <div data-src="/img/patikohv.png" />
       <div data-src="/img/photoonwood.png" />
       <div data-src="/img/done_venice.png" />
